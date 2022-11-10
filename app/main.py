1615ac2902e8e0ed26af4dfe3b6908ccdc62de98
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2022-11-11 01:03:35 +0200

    updated main

--- app/main.py
+++ app/main.py
@@ -1,5 +1,17 @@
+from functions import handler
+
+
 def main():
 
     while True:
 
         command = input("Enter command: ").lower().strip()
+
+        if command in handler:
+            result = handler[command]()
+            if result:
+                print(result)
+            else:
+                continue
+        else:
+            print("Unknown command. Try another one")
