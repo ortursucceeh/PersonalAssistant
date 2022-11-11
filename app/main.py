ca657514095e9f69dc6da9b65981941256cc77d8
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2022-11-11 15:08:43 +0200

    updated main

--- app/main.py
+++ app/main.py
@@ -1,23 +1,24 @@
-from functions import handler
+from functions import handler, user_mistake
 
 
 def main():
 
-    command = input("Enter command: ").lower().strip()
-    if command in handler:
-        result = handler[command]
-        if result:
-            #Here should be real command,and not print.Print just for showing
-            print(result)
-        else:
-            continue
-    else:
-        second_try = user_mistake(command)
-        if second_try:
-            result = handler[second_try]
-            if result:
-            #Here should be real command,and not print.Print just for showing
-                print(result)
-        else:
-           print("Unknown command. Try another one")
+    while True:
+        command = input("Enter command: ").lower().strip()
 
+        if command in handler:
+            result = handler[command]
+            if result:
+                # Here should be real command,and not print.Print just for showing
+                print(result)
+            else:
+                continue
+        else:
+            second_try = user_mistake(command)
+            if second_try:
+                result = handler[second_try]
+                if result:
+                    # Here should be real command,and not print.Print just for showing
+                    print(result)
+            else:
+                print("Unknown command. Try another one")
