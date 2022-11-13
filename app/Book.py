b7dd67feec5ead20b032ff6ca4c088501d9b8549
Yuriy Krasyuk <ykrasyuk@gmail.com>
2022-11-13 12:41:47 +0200

    funcs added
    emailInput
    birthdayInput
    addressInput
    check_email
    check_birthday
    
    __str__class Address
    __str__class Email
    __str__class Birthday
    
    add_email
    remove_email
    add_birthday
    add_address
    remove_address

--- app/Book.py
+++ app/Book.py
@@ -1,12 +1,13 @@
 from collections import UserDict
-from input_checker import NameInput
+from input_checker import nameInput, emailInput, birthdayInput, addressInput
+from datetime import datetime
 
 
 class Book(UserDict):
 
     # work with contact
     def add_contact(self):
-        name = Name(NameInput('name'))
+        name = Name(nameInput('name'))
         phones = None
         email = None
         record = ContactRecord(name, phones, email)
@@ -41,32 +42,78 @@ class Book(UserDict):
 
 # email handling
     def add_email(self):
-        pass
+        name = Name(nameInput(name))
+        email = Email(emailInput(email))
+        self.data[str(name)].email = email
+        return f'Email {str(email)} was added to the Contact "{str(name)}"'
 
-    def change_email(self):
+    def change_email(self):# <----------------------------------- ?
         pass
+        # name = Name(nameInput(name))
+        # old_email = X
+        # new_email = Y
+        # record = self.data[str(name)]
+        # if 
+        #     return f'Email "{old_email}" was changed on "{new_email}" for "{str(name)}"'
+        # else:
+        #     return f'No such email "{old_email} for name "{str(name)}"'
 
     def remove_email(self):
-        name = self.NameInput()
-        self.data[name].email = None
+        name = Name(nameInput(name))
+        self.data[str(name)].email = None
+        return f'Email for the Contact {str(name)} was delleted'
 
 # birthday handling
     def add_birthday(self):
-        pass
+        name = Name(nameInput(name))
+        birthday = Birthday(birthdayInput(birthday))
+        self.data[str(name)].birthday = birthday
+        return f'Birthday date {str(birthday)} was added to the Contact "{str(name)}"'
 
-    def show_birthdays_after(self):
+    def show_birthdays_after(self, N=7): # <----------------------------------- ?
         pass
+        # today_day = datetime.now()
+        # today_year = datetime.now().year
+        # contacts_list = []
+        # for name, record in self.data.items():
+        #     for birthday in record:
+        #         dif = today_day - self.birthday.value.replace(year=today_year)
+        #         if dif >= 0:
+        #             days_number = dif.days
+        #         else:
+        #             dif = self.birthday.value.replace(year=today_year+1) - today_day
+        #             days_number = dif.days
+        #         if days_number == N:
+        #             contacts_list.append(f'{str(name)}: {str(birthday)}\n')
+        #     if contacts_list():
+        #     return f'Contacts for {N} days from today are: \n{contacts_list}'
+        # else:
+        #     return f'No Contacts with birthday in {N} days befor today'
+
 
 # address handling
 
     def add_address(self):
-        pass
+        name = Name(nameInput(name))
+        address = Address(addressInput(address))
+        self.data[str(name)].address = address
+        return f'Address {str(address)} was added to the Contact "{str(name)}"'
 
-    def change_address(self):
+    def change_address(self):# <----------------------------------- ?
         pass
+        # name = Name(nameInput(name))
+        # old_address = X
+        # new_address = Y
+        # record = self.data[str(name)]
+        # if 
+        #     return f'Address "{old_address}" was changed on "{new_adderessl}" for "{str(name)}"'
+        # else:
+        #     return f'No such email "{old_address} for name "{str(name)}"'
 
     def remove_address(self):
-        pass
+        name = Name(nameInput(name))
+        self.data[str(name)].address = None
+        return f'Address for the Contact {str(name)} was delleted'
 
 
 class ContactRecord:
@@ -99,16 +146,20 @@ class Name(ContactField):
 
 
 class Phone(ContactField):
-    pass
+   def __str__(self):
+        return self.value
 
 
 class Email(ContactField):
-    pass
+    def __str__(self):
+        return self.value
 
 
 class Birthday(ContactField):
-    pass
+    def __str__(self):
+        return self.value
 
 
 class Address(ContactField):
-    pass
+    def __str__(self):
+        return self.value
--- app/input_checker.py
+++ app/input_checker.py
@@ -1,7 +1,8 @@
 import re
+from datetime import datetime
 
 
-def NameInput(type_name='name'):
+def nameInput(type_name='name'):
     while True:
         name = input(f'Enter {type_name}: ')
         if check_name(name):
@@ -9,14 +10,41 @@ def NameInput(type_name='name'):
         print('Wrong name')
 
 
-def EmailInput(type_email='email'):
+def emailInput(type_email='email'):
     while True:
         email = input(f'Enter {type_email}: ')
-        if re.match(email).group():
+        if check_email(email):
             return email
-        print('Wrong email format. ')
+        print('Wrong email format.')
+
+
+def birhtdayInput(birthday):
+    while True:
+        birthday = input(f'Enter birthady: ')
+        if check_birthday(birthday):
+            return birthday
+        print('Wrong birhtday format.')
+
+
+def addressInput(address=None):
+    address = input(f'Enter address: ')
+    return address
 
 
 # return True\False
 def check_name(name):
     return bool(name)
+
+
+def check_email(email):
+    if re.fullmatch(r'[a-zA-Z]+[a-zA-Z0-9_.]+@[a-zA-Z]+[.][a-zA-Z]{2,}', email):
+        return email
+    else:
+        raise Exception(f'Please enter correct e-mail address')
+
+
+def check_birthday(birthday):
+    try:
+        birthday = datetime.strptime(birthday, '%d-%m-%Y')
+    except:
+        raise Exception(f'Please enter birthday in format: DD-MM-YYYY')
