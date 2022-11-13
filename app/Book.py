f20a7686ca9b1ed905e016e0cda9b4e1c0b65a5c
Serhii-Kravchenko-2022 <skravchenko20@gmail.com>
2022-11-13 12:36:22 +0200

    Add cont and phone func

--- app/Book.py
+++ app/Book.py
@@ -1,43 +1,96 @@
 from collections import UserDict
-from input_checker import NameInput
+from input_checker import name_input, phone_input, email_input, adsress_input, birtgday_input
 
 
 class Book(UserDict):
 
     # work with contact
     def add_contact(self):
-        name = Name(NameInput('name'))
-        phones = None
-        email = None
-        record = ContactRecord(name, phones, email)
+        name = Name(name_input('name'))
+        phones = Phone(phone_input('phone'))
+        email = Email(email_input('email'))
+        address = Address(adsress_input('address'))
+        birthday = Birthday(birtgday_input('birthday'))
+        for key in self.data:
+            if key == str(name):
+                return f'Contact {str(name)} exist in contacts'
+        record = ContactRecord(name=name, phone=phones, email=email, birthday=birthday, address=address)
         self.data[str(name)] = record
-        return f'Contact {str(name)}'
+        return f'Contact {str(name)} was added to contacts'
 
     def change_contact(self):
-        pass
+        name = Name(name_input('name'))
+        phones = Phone(phone_input('new phone'))
+        email = Email(email_input('new email'))
+        address = Address(adsress_input('new address'))
+        birthday = Birthday(birtgday_input('new birthday'))
+        record = ContactRecord(name=name, phone=phones, email=email, birthday=birthday, address=address)
+        self.data[str(name)] = record
+        return f'Contact {str(name)} was changed in contacts'
 
     def remove_contact(self):
-        pass
+        name = Name(name_input('name'))
+        try:
+            self.data.pop(str(name))
+            return f'Contact {str(name)} was remove from contacts!'
+        except KeyError:
+            return f'Contact {str(name)} not exist. Try again'
 
     def show_contact(self):
-        pass
+        name = Name(name_input('name'))
+        try:
+            record = self.data[str(name)]
+            phones = ', '.join([str(phone.value) for phone in record.phones])
+            return f'Name: {name}\n' \
+                   f'Phone(s): {phones if phones else "-"}\n' \
+                   f'Email: {record.email if record.email else "-"}\n' \
+                   f'Birthday: {record.birthday if record.birthday else "-"}\n' \
+                   f'Address: {record.address if record.address else "-"}'
+        except KeyError:
+            return f'Contact {str(name)} not exist. Try again'
 
     def show_all(self):
         pass
 
     # name handling
-    def change_name(self):
-        pass
+    # def change_name(self):
+    #     pass
 
 # phone handling
     def add_phone(self):
-        pass
+        name = Name(name_input('name'))
+        phone = Phone(phone_input('new phone'))
+        self.data[str(name)].phones.append(phone)
+        return f'Phone {str(phone)} was added to contact {str(name)}'
 
     def change_phone(self):
-        pass
+        name = Name(name_input('name'))
+        old_phone = Phone(phone_input('old phone'))
+        new_phone = Phone(phone_input('new phone'))
+        record = self.data[str(name)]
+        change_flag = False
+        for i, phone in enumerate(record.phones):
+            if str(phone.value) == old_phone.value:
+                record.phones[i] = Phone(new_phone)
+                change_flag = True
+        if change_flag:
+            return f'Phone {old_phone} was changed to {new_phone}'
+        else:
+            return f'{old_phone} is not exist. Try again'
 
     def remove_phone(self):
-        pass
+        name = Name(name_input('name'))
+        old_phone = Phone(phone_input('phone'))
+        record = self.data[str(name)]
+        change_flag = False
+        for i, phone in enumerate(record.phones):
+            if str(phone.value) == old_phone.value:
+                record.phones.remove(phone)
+                change_flag = True
+        if change_flag:
+            return f'Phone {old_phone} was remove from contact {name}'
+        else:
+            return f'{old_phone} is not exist. Try again'
 
 # email handling
     def add_email(self):
@@ -99,7 +152,8 @@ class Name(ContactField):
 
 
 class Phone(ContactField):
-    pass
+    def __str__(self):
+        return self.value
 
 
 class Email(ContactField):
--- app/input_checker.py
+++ app/input_checker.py
@@ -1,12 +1,23 @@
 import re
 
 
-def NameInput(type_name='name'):
+def name_input(type_name='name'):
     while True:
-        name = input(f'Enter {type_name}: ')
+        name = input(f'Enter {type_name}: ').lower()
         if check_name(name):
-            return name
-        print('Wrong name')
+            return name.capitalize()
+        print('You entered an empty string. Try again.')
+
+
+def phone_input(type_name='phone'):
+    while True:
+        phone = input(f'Enter {type_name} like "+38XXXXXXXXXX": ')
+        if not phone:
+            phone = '-'
+            return phone
+        if check_phone(phone):
+            return phone
+        print('Phone number does not match "+38XXXXXXXXXX". Try again.')
 
 
 def EmailInput(type_email='email'):
@@ -20,3 +31,9 @@ def EmailInput(type_email='email'):
 # return True\False
 def check_name(name):
     return bool(name)
+
+
+def check_phone(phone):
+    return bool(re.findall(r'[+38]\d{12}', phone)) and len(phone) == 13
+
+
