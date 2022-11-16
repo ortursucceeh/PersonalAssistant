d6ab779d6a25cc1200626bb57270ff16aab4783f
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2022-11-17 00:14:55 +0200

    changed class methods
    1. moved inputs from class methods to handler
    2. made some output changes
    3. changed docs

--- src/Command_Project_PY4POWER/functions.py
+++ src/Command_Project_PY4POWER/functions.py
@@ -9,30 +9,23 @@ from constants import exit_words, hello_words
 from console_output import show_in_console
 from string import punctuation as punct
 from constants import TRANS, EXTENSIONS, IGNORE_FOLDERS
+from input_checker import *
+
+
 current_directory = os.getcwd()
-saving_folder = os.path.join(
-    current_directory, "saved_data")
+saving_folder = os.path.join(current_directory, "saved_data")
 
 
-def user_mistake(command):
-    posssibilty = difflib.get_close_matches(
-        command, handler.keys(), n=3, cutoff=0.55)
-    if posssibilty:
-        letters = ['1', '2', '3']
-        print("-!- Looks like you make a mistake! -!-")
-        for i in range(len(posssibilty)):
-            print(f"Type '{letters[i]}' if you mean '{posssibilty[i]}'")
-        new_user_input = input("Enter Digit: ").capitalize().strip()
-        while new_user_input not in letters:
-            new_user_input = input(
-                "Enter One of the digits above to choose function: ").capitalize().strip()
-        return posssibilty[letters.index(new_user_input)]
+def hello_func():
+    print('''Hello! I am your personal assistant CLBOT. How can I help you?
+I can handle your contacts and notes. 
+I can filter files by categories in any folder on your computer too. 
+Would you like to look over my full documentation? Then enter the command “show_doc”.''')
 
 
 def start_func():
     global contacts
     global notes
-
     if os.path.exists(os.path.join(saving_folder, "contact_book.pickle")) and \
             os.path.getsize(os.path.join(saving_folder, "contact_book.pickle")):
         with open(os.path.join(saving_folder, "contact_book.pickle"), "rb") as file:
@@ -55,6 +48,27 @@ def start_func():
     return contacts, notes
 
 
+contacts, notes = start_func()
+
+
+def user_mistake(command):
+    posssibilty = difflib.get_close_matches(
+        command, handler.keys(), n=3, cutoff=0.55)
+    if posssibilty:
+        choices = ['1', '2', '3', '']
+        print("-!- Looks like you made a mistake! -!-")
+        for i in range(len(posssibilty)):
+            print(f"\tType '{choices[i]}' if you mean '{posssibilty[i]}'")
+        print("\tOr do not enter anything to skip.")
+        new_input = input(">>> Enter digit: ").strip()
+        while new_input not in choices:
+            new_input = input(
+                ">>> Enter one of the digits above to choose function").strip()
+        if not new_input:
+            return None
+        return posssibilty[choices.index(new_input)]
+
+
 def exit_func():
     if not os.path.exists(saving_folder):
         os.makedirs(saving_folder)
@@ -62,15 +76,18 @@ def exit_func():
         pickle.dump(contacts.data, file)
     with open(os.path.join(saving_folder, "notes.pickle"), 'wb') as file:
         pickle.dump(notes.data, file)
+    print('*' * 61)
     print("See you next time! All contacts and notes were saved locally.")
+    print('*' * 61)
     quit()
 
 
 def show_commands():
+    """Func which shows all commands"""
     print("All commands:")
     keysList = list(handler.keys())
     contact_func = keysList[:16]
-    notes_func = keysList[17:]
+    notes_func = keysList[16:-3]
     other_commands = ["show_commands", "sort_folder",
                       "show_doc", "show_all_contacts", "show_all_notes", 'filter_folder']
     table_headers = ["Contacts Commands", "Notes Commands",
@@ -102,12 +119,8 @@ def show_doc():
         print('\n\n\n')
         print(''.join(data))
 
-
-def hello_func():
-    print('Hello! How can I help you?')
-
-
 # func which return new_filename path
+
 
 def normalize(main_path, file_name):
     name, ext = file_name.stem, file_name.suffix
@@ -186,42 +199,70 @@ def filter_folder():
     return f'Folder {main_path.stem} was successfully filtered!'
 
 
-contacts, notes = start_func()
-
-
 handler = {
-    'add_contact': contacts.add_contact,
-    'change_contact': contacts.change_contact,
-    'remove_contact': contacts.remove_contact,
-    'show_contact': contacts.show_contact,
-    'show_all_contacts': contacts.show_all,
-    'change_name': contacts.change_name,
-    'add_phone': contacts.add_phone,
-    'change_phone': contacts.change_phone,
-    'remove_phone': contacts.remove_phone,
-    'add_email': contacts.add_email,
-    'change_email': contacts.change_email,
-    'remove_email': contacts.remove_email,
-    'add_birthday': contacts.add_birthday,
-    'show_birthdays_after': contacts.show_birthdays_after,
-    'add_address': contacts.add_address,
-    'change_address': contacts.change_address,
-    'remove_address': contacts.remove_address,
-    'add_note': notes.add_note,
-    'change_note': notes.change_note,
-    'remove_note': notes.remove_note,
-    'show_note': notes.show_note,
-    'show_all_notes': notes.show_all,
-    'sort_notes_by': notes.sort_notes_by,
-    'show_notes_with_tag': notes.show_notes_with_tag,
-    'change_title': notes.change_title,
-    'add_keywords': notes.add_keywords,
-    'change_keywords': notes.change_keywords,
-    'remove_keywords': notes.remove_keywords,
-    'add_notedata': notes.add_notedata,
-    'change_notedata': notes.change_notedata,
-    'remove_notedata': notes.remove_notedata,
-    'show_commands': show_commands,
-    'filter_folder': filter_folder,
-    'show_doc': show_doc
+    'add_contact': (contacts.add_contact, (name_input, phones_input,
+                                           email_input, birthday_input, address_input),
+                    ('name', "phone numbers (max 3) separated by space", 'email',
+                    'birthday', 'address')),
+    'change_contact': (contacts.change_contact, (name_input, name_input, phones_input,
+                                                 email_input, birthday_input, address_input),
+                       ('name', 'new phone numbers (max 3) separated by space', 'new email', 'new birthday', 'new address')),
+    'remove_contact': (contacts.remove_contact, (name_input,), ('name',)),
+    'show_contact': (contacts.show_contact, (name_input,), ('name',)),
+    'show_all_contacts': (contacts.show_all,),
+    'change_name': (contacts.change_name, (name_input, name_input),
+                    ('name which you want to change', 'new name')),
+    'add_phone': (contacts.add_phone, (name_input, phones_input),
+                  ('contact`s name for which you want add phones', 'new phones numbers (max 3 in contact)')),
+    'change_phone': (contacts.change_phone, (name_input, phones_input, phones_input),
+                     ('name', 'phone number which you want to change', 'new phone number')),
+    'remove_phone': (contacts.remove_phone, (name_input, phones_input),
+                     ('name', 'phone number for remove')),
+    'add_email': (contacts.add_email, (name_input, email_input),
+                  ('the name for which you want to add email', 'the email to add')),
+    'change_email': (contacts.change_email, (name_input, email_input),
+                     ('the name for which you want to change email', 'the new email')),
+    'remove_email': (contacts.remove_email, (name_input,),
+                     ('the name for which you want to remove email',)),
+    'add_birthday': (contacts.add_birthday, (name_input, birthday_input),
+                     ('the name for which you want to add birthday', 'the birthday date to add')),
+    'show_birthdays_after': (contacts.show_birthdays_after, (daysnumber_input,),
+                             ('days to birthday')),
+    'change_address': (contacts.change_address, (name_input, address_input),
+                       ('the name for which you want to change address', 'the new address')),
+    'remove_address': (contacts.remove_address, (name_input,),
+                       ('the name for which you want to remove address',)),
+
+    # notes handling
+    'add_note': (notes.add_note, (title_input, keywords_input, notedata_input),
+                 ('the title', 'the keywords (max 3)', 'the note data')),
+    'change_note': (notes.change_note, (title_input, title_input, keywords_input, notedata_input),
+                    ('the note title which you want to change',
+                     'the new title', 'the new keywords', 'the new note data')),
+    'remove_note': (notes.remove_note, (title_input,),
+                    ('the note title which you want to remove',)),
+    'show_note': (notes.show_note, (title_input,),
+                  ('the title',)),
+    'sort_notes_by': (notes.sort_notes_by, (category_input,),
+                      ('the category',)),
+    'show_notes_with_tag': (notes.show_notes_with_tag, (tag_input,),
+                            ('the keyword',)),
+    'change_title': (notes.change_title, (title_input, title_input),
+                     ('the note title which you want to change', 'the new note title')),
+    'add_keywords': (notes.add_keywords, (title_input, keywords_input),
+                     ('the note title for which you want to add keywords', 'the keywords')),
+    'change_keywords': (notes.change_keywords, (title_input, keywords_input),
+                        ('the note title for which you want to change the keywords', 'the new keywords')),
+    'remove_keywords': (notes.remove_keywords, (title_input,),
+                        ('the note title for which you want to remove keywords', )),
+    'add_notedata': (notes.add_notedata, (title_input, notedata_input),
+                     ('the note title for which you want to add note data', 'the note data')),
+    'change_notedata': (notes.change_notedata, (title_input, notedata_input),
+                        ('the note title for which you want to change note data', 'the new note data')),
+    'remove_notedata': (notes.remove_notedata, (title_input,),
+                        ('the note title for which you want to remove note data',)),
+    'show_all_notes': (notes.show_all,),
+    'show_commands': (show_commands,),
+    'filter_folder': (filter_folder,),
+    'show_doc': (show_doc,)
 }
